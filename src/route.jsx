fa9de2ebc51969b1c353e7344557898a6be4cb76
DuchPanhathun <duch.panhathun.2821@rupp.edu.kh>
2024-01-29 09:21:50 +0700

    commit

--- src/route.jsx
+++ src/route.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./page/home.jsx";
-import TestPage from "./page/test_page.jsx";
 
 const Page = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/test" element={<TestPage />} />
             </Routes>
         </Router>
     );
